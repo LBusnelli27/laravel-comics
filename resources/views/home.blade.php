e11912728de954e327a6984489d8a2f8bad13083
Busna27 <lucabusnelli2001@gmail.com>
2022-05-04 16:33:08 +0200

    created header and footer section yield

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layouts.base')
+
+@section('header')
+    <h1>Ciao sono nell'header</h1>
+@endsection
+
+
+@section('footer')
+    <h1>Ciao sono nell'footer</h1>
+@endsection
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DC Comics</title>
+</head>
+<body>
+    <header>
+        @yield('header')
+    </header>
+
+
+    <main>
+        @yield('comics')
+    </main>
+
+
+    <footer>
+        @yield('footer')
+    </footer>
+</body>
+</html>
