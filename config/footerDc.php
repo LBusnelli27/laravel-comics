01b1a79af664055744284633ff487eddd7f23a9b
Busna27 <lucabusnelli2001@gmail.com>
2022-05-06 17:04:28 +0200

    footer arrays

---
+++ config/footerDc.php
@@ -0,0 +1,49 @@
+<?php 
+
+return [
+  [
+    "text" => "Terms Of Use",
+    "href" => "#",
+  ],
+  [
+    "text" => "Privacy policy (new)",
+    "href" => "#",
+  ],
+  [
+    "text" => "Ad Choise",
+    "href" => "#",
+  ],
+  [
+    "text" => "Advertising",
+    "href" => "#",
+  ],
+  [
+    "text" => "Jobs",
+    "href" => "#",
+  ],
+  [
+    "text" => "Subscriptions",
+    "href" => "#",
+  ],
+  [
+    "text" => "Talent Workshops",
+    "href" => "#",
+  ],
+  [
+    "text" => "CPSC Certificates",
+    "href" => "#",
+  ],
+  [
+    "text" => "Ratings",
+    "href" => "#",
+  ],
+  [
+    "text" => "Shop Help",
+    "href" => "#",
+  ],
+  [
+    "text" => "Contact Us",
+    "href" => "#",
+  ],
+]
+?>
---
+++ config/footerDcComics.php
@@ -0,0 +1,33 @@
+<?php 
+
+return [
+    [
+        "text" => "Characters",
+        "href" => "#",
+    ],
+    [
+        "text" => "Comics",
+        "href" => "#",
+    ],
+    [
+        "text" => "Movies",
+        "href" => "#",
+    ],
+    [
+        "text" => "Tv",
+        "href" => "#",
+    ],
+    [
+        "text" => "Games",
+        "href" => "#",
+    ],
+    [
+        "text" => "Videos",
+        "href" => "#",
+    ],
+    [
+        "text" => "News",
+        "href" => "#",
+    ],
+]
+?>
